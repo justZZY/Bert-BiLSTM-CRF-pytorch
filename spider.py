bc59a05f0f71d949da87a133dfe73239f58920ce
justZZY <895527626@qq.com>
2020-08-22 22:41:56 +0800

    添加站点与设备的分隔

--- spider.py
+++ spider.py
@@ -14,13 +14,11 @@ def getSiteNames():
                  'grant_type': 'password'}
     # 取token信息
     token_request = requests.post(token_url, data=post_data)
-    print(token_request)
     token_result = json.loads(token_request.content)
     print(token_result)
     authorization = token_result['token_type'] + ' ' + token_result['access_token']
     # 取站点信息
     group_request = requests.get(site_url, headers={'Authorization': authorization, 'X-FBox-ClientId': 'test123456'})
-    print(group_request)
     group_result = json.loads(group_request.content)
     print(group_result)
     site_list = []
@@ -51,6 +49,7 @@ def getSiteNames():
     with open(site_file, 'w', encoding='utf-8') as file_obj:
         for site in site_list:
             file_obj.write(site['name'] + '\n')
+        file_obj.write('|||\n')
         for equip_name in equip_name_list:
             file_obj.write(equip_name + '\n')
 
